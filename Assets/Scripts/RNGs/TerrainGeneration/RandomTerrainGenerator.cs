e28b5db0e008484fd98f2e47652fd38c72d654ab
Jerom Estrada <jerom.estrada7@gmail.com>
2022-10-20 13:19:16 -0700

    setting up for terrain generation

---
+++ Assets/Scripts/RNGs/TerrainGeneration/RandomTerrainGenerator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTerrainGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject _terrainPrefabs;
+    [Header("Terrain Size refers to the number of prefabs along an axis")]
+    [SerializeField] private int _terrainSize;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
