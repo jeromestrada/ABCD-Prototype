66f505d7013f4efb24ebdca17e9caa89107bf41f
Jerom Estrada <jerom.estrada7@gmail.com>
2022-11-07 14:54:16 -0800

    added Require Component to the VFX manager

--- Assets/Scripts/VFX/VFXManager.cs
+++ Assets/Scripts/VFX/VFXManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterStats))]
 public class VFXManager : MonoBehaviour
 {
     private ParticleSystem _shieldEffect;
