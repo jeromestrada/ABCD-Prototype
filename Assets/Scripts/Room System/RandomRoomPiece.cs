b13872c824b8ef2649db0b95e8e4f15af885500c
Jerom Estrada <jerom.estrada7@gmail.com>
2022-11-22 16:32:32 -0800

    added a random room generator using prefabs

---
+++ Assets/Scripts/Room System/RandomRoomPiece.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRoomPiece : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _piecePrefabs = new List<GameObject>();
+
+    private void Start()
+    {
+        GameObject piece = _piecePrefabs[Random.Range(0, _piecePrefabs.Count)];
+        Instantiate(piece, transform.position, Quaternion.Euler(new Vector3(0, 90 * Random.Range(0, 4), 0)));
+        Destroy(gameObject);
+    }
+}
