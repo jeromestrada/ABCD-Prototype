27d00e29860e4e41763e1c3551533204fd6dcd86
Jerom Estrada <jerom.estrada7@gmail.com>
2022-11-29 16:49:45 -0800

    added more prefabs for the random map generation

--- Assets/Scripts/Room System/RandomRoomPiece.cs
+++ Assets/Scripts/Room System/RandomRoomPiece.cs
@@ -6,7 +6,15 @@ public class RandomRoomPiece : MonoBehaviour
 {
     [SerializeField] private List<GameObject> _piecePrefabs = new List<GameObject>();
 
-    private void Start()
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            GeneratePiece();
+        }
+    }
+
+    public void GeneratePiece()
     {
         GameObject piece = _piecePrefabs[Random.Range(0, _piecePrefabs.Count)];
         Instantiate(piece, transform.position, Quaternion.Euler(new Vector3(0, 90 * Random.Range(0, 4), 0)));
