26fff6cbfa9950e4ed864640e3805a26e80ef4b1
Jerom Estrada <jerom.estrada7@gmail.com>
2022-12-06 14:29:36 -0800

    appropriately renamed the hunger system UI

--- Assets/Scripts/Hunger System/HungerSystemUI.cs
+++ Assets/Scripts/Hunger System/HungerSystemUI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HungrySystemUI : MonoBehaviour
+public class HungerSystemUI : MonoBehaviour
 {
     [SerializeField] private HungerSystemHolder _hungerSystemHolder;
     [SerializeField] private Image _hungerMeter;
