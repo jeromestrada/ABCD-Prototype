72804a474fc38815b5d7010851cd0ced474b1794
Jerom Estrada <jerom.estrada7@gmail.com>
2023-01-14 15:45:01 -0800

    trying to fix the isometric movement issue

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent agent; // this will move the player through offmeshlinks, allowing the player to pass between rooms using gates
     [SerializeField] private PlayerStats myStats;
+    CharacterController controller;
 
     public Vector3 move;
     public Vector3 dashForward;
@@ -28,8 +29,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] bool isGrounded;
     [SerializeField] Vector3 velocity;
 
+    public Quaternion currentRotation;
+    public Quaternion targetRotation;
     float turnSmoothness = 0.1f;
     float turnSmoothVelocity;
+    float turnRate = 10f;
 
     PlayerCombat combat;
     public bool isAttacking = false;
@@ -61,6 +65,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        controller = GetComponent<CharacterController>();
         scanner = GetComponent<InteractableScanner>();
         combat = GetComponent<PlayerCombat>();
         maxSpeed = myStats.Movespeed.GetValue();
@@ -77,8 +82,26 @@ public class PlayerMovement : MonoBehaviour
         {
             velocity.y = -1f;
         }
-        CharacterController controller = GetComponent<CharacterController>();
+
+        HandleRotation();
         HandleMovement(controller);
+    }
+
+    void HandleRotation()
+    {
+        Vector3 positionToLookAt;
+
+        positionToLookAt.x = move.x;
+        positionToLookAt.y = 0f;
+        positionToLookAt.z = move.z;
+
+        currentRotation = transform.rotation;
+
+        if(move.magnitude > 0.1f)
+        {
+            targetRotation = Quaternion.LookRotation(positionToLookAt);
+            transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, turnRate * Time.deltaTime);
+        }
     }
 
     void HandleMovement(CharacterController controller)
@@ -93,14 +116,15 @@ public class PlayerMovement : MonoBehaviour
         forward = forward.normalized;
         right = right.normalized;
 
-        Vector3 forwardRelativeVerticalInput = verticalInput * forward;
-        Vector3 rightRelativeVerticalInput = horizontalInput * right;
+        Vector3 forwardRelativeVerticalInput = (verticalInput * forward).normalized;
+        Vector3 rightRelativeVerticalInput = (horizontalInput * right).normalized;
 
-        move = forwardRelativeVerticalInput + rightRelativeVerticalInput;
+        move = (forwardRelativeVerticalInput + rightRelativeVerticalInput).normalized;
+        //move = new Vector3(horizontalInput, 0f, verticalInput);
 
-        targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg;
+        /*targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg;
         angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothness);
-        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);*/
 
         velocity.y += gravity * Time.deltaTime;
         if (move.magnitude >= 0.1f)
